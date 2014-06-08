ad6df12dbf34b5ff81d1a31d9ffdbbf1848bb336
RockyF <rockyf@126.com>
2014-06-09 01:03:26 +0800

    ai

--- src/control/AIController.ts
+++ src/control/AIController.ts
@@ -2,25 +2,43 @@
  * Created by lenovo on 2014/6/6.
  */
 
+///<reference path="../egret.d.ts"/>
+
 class AIController{
+	private static _instance:AIController;
+	public static getInstance():AIController{
+		return this._instance ? this._instance : this._instance = new AIController();
+	}
+
 	rate:number;
+
+	objects:Array;
+
 	constructor(rate:number = 500){
 		this.rate = rate;
+		this.objects = [];
 	}
 
 	start():void{
+		egret.Ticker.getInstance().register(this.update, this);
+	}
 
+	update():void{
+		var item:AIObject;
+		for(var i = 0, len = this.objects.length; i< len; i++){
+			item.step();
+		}
 	}
 
 	pause():void{
-
+		egret.Ticker.getInstance().unregister(this.update, this);
 	}
 
 	resume():void{
-
+		egret.Ticker.getInstance().register(this.update, this);
 	}
 
 	stop():void{
-
+		egret.Ticker.getInstance().unregister(this.update, this);
 	}
 }
