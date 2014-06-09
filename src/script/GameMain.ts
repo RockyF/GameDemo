0bb24908fcb426d454a030a601ae92818c04ece7
RockyF <rockyf@126.com>
2014-06-09 18:29:32 +0800

    Game AI

---
+++ src/script/GameMain.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by lenovo on 2014/6/9.
+ */
+
+///<reference path="../support/IAiScript.ts"/>
+
+class game_main implements IAiScript{
+	onCreate():void {
+	}
+
+	onDestroy():void {
+	}
+
+	onStep():void {
+	}
+
+}
